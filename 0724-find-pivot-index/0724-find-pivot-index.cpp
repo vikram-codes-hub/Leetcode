b6b387db23a3786a57b65e379a2b2fa10f7ee894
Vikram Singh gangwar <vikramsingh9475889367@gmail.com>
2026-07-02 04:14:41 +0530

    Time: 0 ms (100%), Space: 35.8 MB (56.03%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int ls=0,total=0;
+        int t=0,n=nums.size(),ls=0;
         for(auto i:nums){
-            total+=i;
+            t+=i;
         }
 
-        for(int i=0;i<nums.size();i++){
-            int rs=total-ls-nums[i];
-            if(rs==ls){
-                return i;
-            }else ls+=nums[i];
+        for(int i=0;i<n;i++){
+             int rs=t-nums[i]-ls;
+             if(ls==rs)return i;
+             ls+=nums[i];
         }
         return -1;
     }
 };
-
-/*
-ls=0 ts=28 
-i=0->rs=ts-ls-cuur   28-0-1 no
-*/
