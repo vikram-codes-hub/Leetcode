1ed844bd40762e0e2c2c36a5b4793a7c5df70589
Vikram Singh gangwar <vikramsingh9475889367@gmail.com>
2026-07-06 03:46:25 +0530

    Time: 125 ms (10.97%), Space: 182.1 MB (39.79%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,43 +1,46 @@
 class LRUCache {
 public:
-unordered_map<int,pair<list<int>::iterator,int>>mp;
-list<int>dll;
-int n;
+    unordered_map<int,pair<list<int>::iterator,int>>mp;
+    list<int>dll;
+    int n;
     LRUCache(int capacity) {
-        n=capacity;  
+        n=capacity;
     }
-    void makemostrectn(int key){
-    //ye function use kra hue key ko sabse agge le atta haui-->sabse phele key ko delte kro piche se  then key ko agge insert kro then address update kro key ka masp;
-
-    dll.erase(mp[key].first);
-    dll.push_front(key);
-    mp[key].first=dll.begin();
-}
+    void makemost(int key){
+        //sbse phele address pr jao aur usko remove kr do 
+        dll.erase(mp[key].first);
+        //ab usko sabse age dal do;
+        dll.push_front(key);
+        //update address in the map;
+        mp[key].first=dll.begin();
+    }
     int get(int key) {
+        //in this first see if we ahve the value in the amp if not return-1
         if(mp.find(key)!=mp.end()){
-            makemostrectn(key);
+
+            //sbase phele agge le aoo
+            makemost(key);
             return mp[key].second;
         }
         return -1;
     }
     
     void put(int key, int value) {
-        //sbse phele check kya key already present ahi ya nhi map me if yes update the val;ue of it
+        //check if the key is present in the map,if yes we just need tonudpate the address,value  nothing else
         if(mp.find(key)!=mp.end()){
-            makemostrectn(key);
-            mp[key]={dll.begin(),value};
-            return;
+          mp[key].second=value;
+          makemost(key);
+          return;
         }
-        //mtlb key present nhi hai to isnert kro fresh
+        //agr nhi ahi to push kro
         dll.push_front(key);
         mp[key]={dll.begin(),value};
         n--;
 
         if(n<0){
-           int key_to_delete=dll.back();
+            int key_to_del=dll.back();
             dll.pop_back();
-            mp.erase(key_to_delete);
-            n++;
+            mp.erase(key_to_del);
         }
     }
 };
