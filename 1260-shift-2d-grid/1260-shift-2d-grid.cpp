a79363e6b4086924ead7d3af810b5d36716a74ac
Vikram Singh gangwar <vikramsingh9475889367@gmail.com>
2026-07-20 23:39:37 +0530

    Time: 60 ms (5.16%), Space: 46.6 MB (5.16%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -8,21 +8,21 @@ public:
 
         for (int i = 0; i < k; i++) {
 
-            vector<vector<int>> ans(n, vector<int>(m, 0)); // Reset every shift
+            vector<vector<int>> ans(n, vector<int>(m, 0)); 
 
             for (int l = 0; l < n; l++) {
 
-                // Shift elements within the row
+               
                 for (int p = 0; p < m - 1; p++) {
                     ans[l][p + 1] = lll[l][p];
                 }
 
-                // Last element of row goes to first column of next row
+             
                 if (l != n - 1)
                     ans[l + 1][0] = lll[l][m - 1];
             }
 
-            // Last element of grid goes to first position
+         
             ans[0][0] = lll[n - 1][m - 1];
 
             lll = ans;
