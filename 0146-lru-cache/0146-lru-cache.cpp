6184d18dcd9f29689c341997c5a50c03bf52a43c
Vikram Singh gangwar <vikramsingh9475889367@gmail.com>
2026-08-02 04:02:34 +0530

    Time: 111 ms (20.29%), Space: 182.2 MB (28.32%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,43 +1,41 @@
 class LRUCache {
 public:
-unordered_map<int,pair<list<int>::iterator,int>>mp;
-list<int>cache;
+unordered_map<int, pair<list<int>::iterator, int>> cache;
+list<int>ll;
 int n;
     LRUCache(int capacity) {
-         n=capacity;
+        n=capacity;
     }
-    void maketofirst(int key){
-        cache.erase(mp[key].first);
-        cache.push_front(key);
-        mp[key].first=cache.begin();
-    }
+    void makerecent(int key){
+       ll.erase(cache[key].first);
+        ll.push_front(key);
+        cache[key].first = ll.begin();
 
-    
+    }
     int get(int key) {
-        if(mp.find(key)!=mp.end()){
-            //sbase phele agge dalo usko dll me fhir uske bad uska address chane kro fhir uska value return krdo
-            maketofirst(key);
-            return mp[key].second;
+        if(cache.find(key)!=cache.end()){
+            makerecent(key);
+            return cache[key].second;
         }
         return -1;
     }
     
     void put(int key, int value) {
-        //check ki khai mey presengt to nhi
-        if(mp.find(key)!=mp.end()){
-            mp[key].second=value;
-            maketofirst(key);         
-        }else{
-            cache.push_front(key);
-            mp[key]={cache.begin(),value};
-            n--;
+        if(cache.find(key)!=cache.end()){
+             makerecent(key);
+             cache[key]={ll.begin(),value};
+             return;
         }
 
-        if(n<0){//mtlb  size se ajda exceed kr gyi hai
-        int key_to_del=cache.back();
-        mp.erase(key_to_del);
-        cache.pop_back();
-        n++;
+        ll.push_front(key);
+        cache[key]={ll.begin(),value};
+        n--;
+
+        if(n<0){
+            int key_to_del=ll.back();
+            cache.erase(key_to_del);
+            ll.pop_back();
+            n++;
         }
     }
 };
